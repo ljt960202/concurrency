cb4a54a48e0ec03729014eef79e57010a963007e
Duplicator <ljt199622ljt@163.com>
2019-10-16 16:59:39 +0800

    第一次推送

---
+++ src/main/java/com/leejt/concurrency/ConcurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.leejt.concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyApplication.class, args);
+	}
+
+}
